585b33c8c3967481a5b810b9c20924ba6c5ec3ba
Bijesh Neupane <neubij64@gmail.com>
2025-04-21 18:22:32 +0545

    setup navabar and authentication pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Authentication from "./pages/Authentication";
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 const App = () => {
-  return <div>App</div>;
+  const [openAuth, setOpenAuth] = useState(false);
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar setOpenAuth={setOpenAuth} />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+          {openAuth && (
+            <Authentication openAuth={openAuth} setOpenAuth={setOpenAuth} />
+          )}
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App;
